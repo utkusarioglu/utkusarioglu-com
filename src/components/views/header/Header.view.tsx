faa0a67bf36eb94bc4d689474829689808f33e84
utkusarioglu <utkusarioglu@hotmail.com>
2020-12-26 12:15:13 +0300

    Feat: Implement header

---
+++ src/components/views/header/Header.view.tsx
@@ -0,0 +1,13 @@
+import './Header.view.scss';
+
+const HeaderView = () => {
+  return (
+    <header {...{ className: 'header' }}>
+      <div {...{ className: 'header__title' }}>
+        <h1>Utku Sarioglu</h1>
+      </div>
+    </header>
+  );
+};
+
+export default HeaderView;
