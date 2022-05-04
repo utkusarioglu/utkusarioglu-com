618377249b98f00842aab3c580bae92293e882a0
Utku Sarioglu <32932503+utkusarioglu@users.noreply.github.com>
2022-05-04 18:41:38 +0300

    AWS updates (#12)
    * Fix color bugs for PaperChainListItem
    
    PaperChainListItem.view.scss
    - Move color styling out of the module. This styling will
      be placed in theme styling of the repo.
    
    _default.scss
    - Fix class names for `no-items-indicator` and `loading-indicator`.
      these two styles were failing as their class names were excluded
      from the class name changes in prior commits
    - Move the `background-color` styling for `paper-chain-list-item` into
      its related section.
    
    * Alter triggers for public data actions
    
    - Remove `main` branch triggers for trucker jokes and paper chain.
      This is done because when main receives a pr, it will build and
      publish the entire code anyway, there is no need for these
      triggers to contain `main` while publish.yml does also does this.
    
    * Update paper chain
    
    * Update paper chain
    
    * Update ArtsyFartsy page title
    
    - Add "Artsy Fartsy" to route title.
    
    * Update ArtsyFartsy svg
    
    - Add some more shapes in the page's design.
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update PaperChainList.view
    
    - Update CSS properties of the hud to allow the text size in the
      hud to remain fixed while the content text changes in size.
      This change is done to allow today's chain to display as expected.
    
    * Refactor usePaperChainList
    
    paper-chain-list.hooks.ts
    - Split the file name and path. This is done to prepare the method
      for file name being created by a function for paginated fetches.
    
    * Update paper chain for 20220329
    
    * Create script for adding stub in paper-chain
    
    modified:   .gitignore
    - Remove vscode from ignore list. This is done to include the
      new task in the repo
    
    new file:   .vscode/tasks.json
    - Add add link script as a task for vscode
    
    modified:   package.json
    - Create script entry for adding link for paper-chain
    
    new file:   scripts/add-new-paper-chain-link.js
    - Create script for adding a new stub to paper-chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper chain
    
    * Update paper-chain
    
    * Update paper-chain
    
    * Update paper-chain
    
    * Update paper chain
    
    * Update paper-chain
    
    * Update paper chain
    
    * Update general typography
    
    styles/base/_typography.scss
    - Increase line height to 1.4em for h1 through span. This is effective
      everywhere but a dense amount of text is only available in
    `paper-chain`. And this change is done in service of that page.
    
    * Chore: Update actions for aws transition
    
    aws.yml
    - Create aws action for pushing to an s3 bucket. This action is at test phase
    
    build-web.yml
    - Create build action that creates an artifact rather than pushing to
      the ftp server, which was always a bad idea as one job was handling both
      build and push.
    
    publish.yml
    - Remove action, this is succeeded by `build-web`.
    
    update-paper-chain.yml
    update-trucker-jokes.yml
    - Disable triggers for these actions until actions stabilize in their new
      versions.
    
    * Chore: rename action, remove commented config
    
    .github/workflows/build-web.yml
    - Rename the job, the old name was from the previous build
      job and it does not reflect the behavior of this job.
    - Remove commented FTP upload configuration. It will not be needed.
    
    * Chore: change name of job in aws

---
+++ scripts/add-new-paper-chain-link.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+const { readFile, writeFile } = require("fs/promises");
+const path = require("path");
+
+const { signal, abort } = new AbortController();
+
+async function readHistory(filename) {
+  try {
+    const file = await readFile(filename, { signal, encoding: "utf-8" });
+    return JSON.parse(file);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+/**
+ * Creates a standard new entry with timestamp of "now"
+ */
+function createNewEntry() {
+  return {
+    content: "STUB",
+    timestamp: Date.now(),
+    style: {
+      backgroundColor: "black",
+      color: "white",
+    },
+  };
+}
+
+/**
+ * Includes the new entry inside the read file
+ */
+function addNewEntry(file, newEntry) {
+  return {
+    ...file,
+    list: [newEntry, ...file.list],
+  };
+}
+
+async function writeUpdatedFile(filename, content) {
+  try {
+    const stringified = JSON.stringify(content, null, 2);
+    await writeFile(filename, stringified);
+    console.log(`Added new stub to ${filename}`);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+/**
+ * Main function that handles the entire operation
+ */
+async function main() {
+  const dataPath = "../public/paper-chain-data";
+  const filename = path.join(__dirname, dataPath, "2020-01.json");
+  const file = await readHistory(filename);
+  const newEntry = createNewEntry();
+  const updated = addNewEntry(file, newEntry);
+  await writeUpdatedFile(filename, updated);
+}
+
+process.on("SIGTERM", () => {
+  console.log("Terminating");
+  abort();
+});
+
+main();
