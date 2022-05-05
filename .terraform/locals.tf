cae895c81f0394137effb9559d79bd91c961d993
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-05-05 16:46:19 +0300

    Chore: update tf config
    .terraform/locals.tf
    - Add new variables for tags, mime types and upload list. Upload list
    
    .terraform/s3-bucket.tf
    - Update `for_each` list to use `local.upload_list`
    - Add `content_type` property to set correct mime types for the
      objects

--- .terraform/locals.tf
+++ .terraform/locals.tf
@@ -1,4 +1,11 @@
 locals {
-  build_dir = "../build"
+  default_tags = {
+    Repo = "utkusarioglu-com"
+    StateStore = "arn:aws:s3:::utkusarioglu-com-state-store"
+    Admin = "aws-automation-setup.utkusarioglu.github"
+  }
+  build_path = "../build"
   bucket_name = "utkusarioglu-com-static-content"
+  mime_types = jsondecode(data.http.mime_types.body)
+  upload_list = fileset("${local.build_path}/", "**")
 }
--- .terraform/s3-bucket.tf
+++ .terraform/s3-bucket.tf
@@ -1,8 +1,9 @@
 resource "aws_s3_object" "static_content" {
   provider = aws.us_west_2
-  for_each = fileset(local.build_dir, "**")
+  for_each = local.upload_list
   bucket = local.bucket_name
   key = each.value
   source = "${local.build_dir}/${each.value}"
   etag = filemd5("${local.build_dir}/${each.value}")
+  content_type = lookup(local.mime_types, regex("\\.[^.]+$", each.value), null)
 }
