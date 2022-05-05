23c6d5c84d7188f35c8c60d0d95ab43268f08b3b
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-05-05 17:43:37 +0300

    Chore: revert to non-test state
    modified:   .github/workflows/aws.yml
    modified:   .terraform/locals.tf
    - Re-enable build-web, re-enable using the build artifacts
    
    deleted:    build-test/error.html
    deleted:    build-test/index.html
    - These were test files. They are not needed anymore.

--- .terraform/locals.tf
+++ .terraform/locals.tf
@@ -4,8 +4,7 @@ locals {
     StateStore = "arn:aws:s3:::utkusarioglu-com-state-store"
     Admin = "aws-automation-setup.utkusarioglu.github"
   }
-  # build_path = "../build"
-  build_path = "../build-test"
+  build_path = "../build"
   bucket_name = "utkusarioglu-com-static-content"
   mime_types = jsondecode(data.http.mime_types.body)
   upload_list = fileset("${local.build_path}/", "**")
