01fda0ebfca7ad7f0c91be5b82e1ca1eae870291
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-05-05 17:21:31 +0300

    Chore: switch to `build-test` for gh actions tests
    Because of multiple unexpected errors stalling cd
    infrastructure build, we are switching to a lightweigt
    pipeline to quickly test out ideas. The config and
    some of the files introduced in this commit will later
    be deleted.

--- .terraform/locals.tf
+++ .terraform/locals.tf
@@ -4,7 +4,8 @@ locals {
     StateStore = "arn:aws:s3:::utkusarioglu-com-state-store"
     Admin = "aws-automation-setup.utkusarioglu.github"
   }
-  build_path = "../build"
+  # build_path = "../build"
+  build_path = "../build-test"
   bucket_name = "utkusarioglu-com-static-content"
   mime_types = jsondecode(data.http.mime_types.body)
   upload_list = fileset("${local.build_path}/", "**")
