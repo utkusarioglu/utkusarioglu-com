799a88ee2468928353cf948a1734cad9b02882d5
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-05-05 15:24:03 +0300

    Chore: fix tf s3 sync resource
    .terraform/s3-bucket.tf
    - Fix bucket name in s3_object resource. This name issue was causing the
      gh actions runs to fail

--- .terraform/s3-bucket.tf
+++ .terraform/s3-bucket.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_object" "static_content" {
   provider = aws.us_west_2
   for_each = fileset(local.build_dir, "**")
-  bucket = aws_s3_bucket.state_store.bucket
+  bucket = aws_s3_bucket.static_content.bucket
   key = each.value
   source = "${local.build_dir}/${each.value}"
   etag = filemd5("${local.build_dir}/${each.value}")
