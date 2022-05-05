0a1ea8ad4f282faf01eb6b7b0481780a02c853a0
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-05-05 16:56:03 +0300

    Chore: fix tf local var name issue
    .terraform/s3-bucket.tf
    - Fix s3 bucket resource `build_dir` variable names. The correct names
      are `build_path`.

--- .terraform/s3-bucket.tf
+++ .terraform/s3-bucket.tf
@@ -3,7 +3,7 @@ resource "aws_s3_object" "static_content" {
   for_each = local.upload_list
   bucket = local.bucket_name
   key = each.value
-  source = "${local.build_dir}/${each.value}"
-  etag = filemd5("${local.build_dir}/${each.value}")
+  source = "${local.build_path}/${each.value}"
+  etag = filemd5("${local.build_path}/${each.value}")
   content_type = lookup(local.mime_types, regex("\\.[^.]+$", each.value), null)
 }
