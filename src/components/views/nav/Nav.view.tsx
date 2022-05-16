3654f49c13a6c3d3f6414312b46da7a40140cb76
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-05-16 09:18:17 +0000

    Feat: Update nav styling
    src/components/views/nav/Nav.view.scss
    src/components/views/nav/Nav.view.tsx
    - Refactor `navItems` as `SOCIALS`
    - Add `NAV_ROUTES` for internal routes.
    - Update css class names to be a bit more compatible with BEM, there
      is still much work to be done on this front.

--- src/components/views/nav/Nav.view.tsx
+++ src/components/views/nav/Nav.view.tsx
@@ -1,17 +1,24 @@
-import { navItems } from "./Nav.constants";
+import { SOCIALS, NAV_ROUTES } from "../../../config";
 import ReactGa from "react-ga";
+import { Link } from "react-router-dom";
 // import "./Nav.view.scss";
 
 const NavView = () => {
   return (
-    <div {...{ className: "nav" }}>
+    <div {...{ className: "nav-container" }}>
       <nav>
-        {navItems &&
-          navItems.map(({ link, title }) => {
+        {NAV_ROUTES &&
+          NAV_ROUTES.map(({ link, title }) => (
+            <Link to={link} className="nav-link nav-link-route">
+              {title}
+            </Link>
+          ))}
+        {SOCIALS &&
+          SOCIALS.map(({ link, title }) => {
             return (
               <ReactGa.OutboundLink
                 {...{
-                  className: "nav__link",
+                  className: "nav-link nav-link-social",
                   key: title,
                   eventLabel: `${title}Visit`,
                   to: link,
