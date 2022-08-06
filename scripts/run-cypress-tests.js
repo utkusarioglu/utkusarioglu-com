cafc8575e52f84968f05a7bad36f35279515863b
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-08-06 06:33:35 +0000

    Chore(actions): Limit browser types in Cypress
    - Reduce browser types to only Chrome for now to speedily test
      the Cypress testing pipeline. The previous code is left as commented
      as it is expected to be reused later.

--- scripts/run-cypress-tests.js
+++ scripts/run-cypress-tests.js
@@ -2,7 +2,9 @@
 const cypress = require("cypress");
 const config = require("../cypress.config.js");
 
-const BROWSERS = ["chrome", "firefox", "edge"];
+// This next line is expected to be reused
+// const BROWSERS = ["chrome", "firefox", "edge"]
+const BROWSERS = ["chrome"];
 
 BROWSERS.reduce((chain, browser) => {
   chain = chain.then(() => {
