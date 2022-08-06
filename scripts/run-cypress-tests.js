f37ebe82c0c9072cd84cd0a08290dc1248f36255
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-08-06 06:46:12 +0000

    Chore(cypress): Enable all browsers
    - Enable all browsers after it was disabled for speedy testing of the
      cypress pipeline.

--- scripts/run-cypress-tests.js
+++ scripts/run-cypress-tests.js
@@ -2,9 +2,7 @@
 const cypress = require("cypress");
 const config = require("../cypress.config.js");
 
-// This next line is expected to be reused
-// const BROWSERS = ["chrome", "firefox", "edge"]
-const BROWSERS = ["chrome"];
+const BROWSERS = ["chrome", "firefox", "edge"];
 
 BROWSERS.reduce((chain, browser) => {
   chain = chain.then(() => {
