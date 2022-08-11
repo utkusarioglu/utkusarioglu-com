3b7f5d3278d6923c76bf62f88a2b0aa2f639dc21
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-08-11 09:52:54 +0000

    Test(home.cy.js): Restructure tests
    - Add `describe` for testing themes at 30 seconds. This test uses
      local storage value to set dark and light mode.
    - Adjust latencies in other tests

--- .cypress/e2e/home.cy.js
+++ .cypress/e2e/home.cy.js
@@ -5,11 +5,33 @@ const pages = ["About", "Resume", "Musings", "Canvas"];
 const links = ["LinkedIn", "Instagram", "Twitter"];
 
 Cypress.env().viewportSizes.forEach((viewportSize) => {
-  describe(describeName(Cypress, viewportSize), () => {
+  describe(
+    describeName(Cypress, viewportSize, "Themes from localStorage"),
+    () => {
+      const commonActions = () => {
+        cy.viewport(...viewportSize);
+        cy.visit(url);
+        cy.wait(30000);
+        cy.screenshot();
+      };
+
+      it("Should render light theme background as expected", () => {
+        commonActions();
+        cy.get("html").should("not.have.class", "dark");
+      });
+
+      it("Should render light theme background as expected", () => {
+        window.localStorage.setItem("theme", "dark");
+        commonActions();
+        cy.get("html").should("have.class", "dark");
+      });
+    }
+  );
+
+  describe(describeName(Cypress, viewportSize, "navigation"), () => {
     beforeEach(() => {
       cy.viewport(...viewportSize);
       cy.visit(url);
-      cy.wait(5000);
     });
 
     it("Should contain expected links", () => {
@@ -22,7 +44,7 @@ Cypress.env().viewportSizes.forEach((viewportSize) => {
     pages.forEach((page) => {
       it(`Should navigate to ${page} page`, () => {
         cy.contains(page).click();
-        cy.wait(1000);
+        cy.wait(2000);
         cy.screenshot();
       });
     });
