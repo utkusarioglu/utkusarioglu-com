d4d14b0e28be2b5bd0825d350cb1667cdefc1843
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-08-12 05:13:10 +0000

    Chore: Update cypress container to mount certs
    - Update Cypress container to mount the certs folder. The certs
      folder part is read from `.env` file. However this solution seems
      very hacky. This entire chain for cypress ignores that Cypress
      offers github actions.

--- scripts/docker-run-cypress.sh
+++ scripts/docker-run-cypress.sh
@@ -9,6 +9,8 @@ docker run \
   -v $(pwd)/.cypress:$WORKDIR/cypress \
   -v $(pwd)/scripts:$WORKDIR/scripts \
   -v $(pwd)/cypress.config.js:$WORKDIR/cypress.config.js \
+  -v $CERTS_FOLDER/server:$WORKDIR/.certs/server  \
+  -v $CERTS_FOLDER/root:$WORKDIR/.certs/root  \
   -w $WORKDIR \
   --env-file $(pwd)/.env \
   --name utkusarioglu-com-cypress-tests \
