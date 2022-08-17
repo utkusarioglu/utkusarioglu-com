b61ce02e32b26b3a81a53f4b4a9194a6752254da
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-08-17 14:33:31 +0000

    Feat(img): Set max responsive size for content img
    - Set `MAX_W_PROSE` as the `maxResponsiveWidth` value for
      content images.

--- src/pages/musings.page.tsx
+++ src/pages/musings.page.tsx
@@ -3,6 +3,7 @@ import ContentLayout from "_layouts/content/Content.layout";
 import EnhancedImage from "_primitives/enhanced-image/EnhancedImage.primitive";
 import Paragraph from "_primitives/paragraph/Paragraph.primitive";
 import Hyperlink from "_primitives/hyperlink/Hyperlink.primitive";
+import { MAX_W_PROSE } from "_constants";
 
 const content = [
   "Quisque at enim suscipit, luctus felis sed, vehicula risus. Fusce sit amet diam consequat, viverra quam in, consectetur mi. Praesent lectus elit, convallis non metus et, sodales feugiat sapien. Donec neque urna, rutrum at risus a, malesuada tempus ex. Mauris ultrices massa sem, pretium tincidunt risus finibus eu. Aliquam erat volutpat. Nullam pharetra lacus eget fermentum congue. In hac habitasse platea dictumst. Sed venenatis nibh non quam ultricies gravida. Ut porta, dolor eget fermentum porta, massa diam posuere ante, in consequat ligula turpis et eros. Curabitur scelerisque tellus in sem iaculis accumsan. Vestibulum viverra nunc auctor semper lacinia. Nulla facilisi. Donec sed enim molestie, tempus justo in, ornare ex. Nulla faucibus tortor a nulla sodales, in pellentesque nisl ornare. ",
@@ -18,6 +19,7 @@ const AboutPage: FC<AboutPageProps> = () => {
         alt="Musings title image"
         credits="photo by Utku Sarioglu"
         src={require("_assets/images/old-ship.jpg")}
+        maxResponsiveWidth={MAX_W_PROSE}
       />
       <Paragraph>
         Click here for the
