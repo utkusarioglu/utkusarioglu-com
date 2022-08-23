1863e85f9a01772acf880a1bc4e026b3e0524fdd
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-08-23 18:52:20 +0300

    Chore(terraform): Adapt main to only serve `main`
    - Alter `data.mime_types` to point to the latest gist.
    - Add new default tag `Branch` to specify with branch the
      resources belong to. This will be important when the `dev` branch
      starts to push content.
    - Update `build_path` to point one level above. This is required as
      the module is now one level deeper than the project root.
    - Rename `local.buckets` to `local.bucket` as this module only
      serves a single subdomain, `main`.
    - Remove `var.tf` as this module will not be receiving a git branch
      name anymore.

--- .terraform/main/s3-bucket.tf
+++ .terraform/main/s3-bucket.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_object" "static_content" {
   provider = aws.us_west_2
   for_each = local.upload_list
-  bucket = local.buckets.main
+  bucket = local.bucket
   key = each.value
   source = "${local.build_path}/${each.value}"
   etag = filemd5("${local.build_path}/${each.value}")
