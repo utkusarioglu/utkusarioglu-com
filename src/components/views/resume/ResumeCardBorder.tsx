3eb72a45a6b0fac73900bd8df30ea113d8a77ad4
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-08-23 08:31:04 +0000

    Feat(resume): Update card color
    - Update `backgroundColor` for `ResumeCardBorder`. This update
      means that the component now uses a value from the `constants`
      module instead of defining its own.

--- src/components/views/resume/ResumeCardBorder.tsx
+++ src/components/views/resume/ResumeCardBorder.tsx
@@ -8,8 +8,7 @@ interface ResumeCardBorderProps {
 const ResumeCardBorder: FC<ResumeCardBorderProps> = ({ children }) => (
   <div
     className={[
-      COLORS.canvasControlBorder,
-      "bg-neutral-600/20",
+      COLORS.canvasControlInput,
       "px-5 py-3 mb-3 last:mb-0 rounded-lg",
     ].join(" ")}
   >
