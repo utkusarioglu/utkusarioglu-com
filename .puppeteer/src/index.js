c8e456bf04770cd7aca9ffe0685c29db84357238
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-08-28 12:29:39 +0000

    Chore(pdf): Implement puppeteer code for pdf
    - Implement puppeteer code for creating raw pdf files.

---
+++ .puppeteer/src/index.js
@@ -0,0 +1,56 @@
+const puppeteer = require("puppeteer");
+
+const artifactsPath = "/home/pptruser/artifacts";
+const url = "https://www.utkusarioglu.com/resume";
+
+const variants = [
+  {
+    format: "a4",
+    margin: {
+      x: 50,
+      y: 55,
+    },
+  },
+  {
+    format: "letter",
+    margin: {
+      x: 40,
+      y: 40,
+    },
+  },
+];
+
+(async () => {
+  const browser = await puppeteer.launch({
+    args: [
+      // TODO get rid of this
+      "--ignore-certificate-errors",
+    ],
+  });
+  const page = await browser.newPage();
+  page.setViewport({ width: 1920, height: 1080 });
+  await page.goto(url, {
+    waitUntil: "networkidle2",
+  });
+  await page.screenshot({ path: `${artifactsPath}/screenshots/resume.png` });
+
+  await variants.reduce(async (chain, { format, margin }) => {
+    chain = chain.then(() =>
+      page.pdf({
+        displayHeaderFooter: false,
+        omitBackground: true,
+        path: `${artifactsPath}/raw/resume-${format}.pdf`,
+        format,
+        margin: {
+          left: margin.x,
+          right: margin.x,
+          bottom: margin.y,
+          top: margin.y,
+        },
+      })
+    );
+    return chain;
+  }, Promise.resolve());
+
+  await browser.close();
+})();
