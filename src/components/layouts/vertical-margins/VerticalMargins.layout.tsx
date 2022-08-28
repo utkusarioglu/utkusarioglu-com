c0c714996f0f86143d2ed3de1960b20563ac819a
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-08-28 12:24:03 +0000

    Feat(layout): Alter `VerticalMarginsLayout`
    - Add `className` prop to the component in order to customize its
      style. This will be needed in resume page.

--- src/components/layouts/vertical-margins/VerticalMargins.layout.tsx
+++ src/components/layouts/vertical-margins/VerticalMargins.layout.tsx
@@ -2,10 +2,12 @@ import { type FC, type ReactNode } from "react";
 
 interface VerticalMarginsLayoutProps {
   children: ReactNode;
+  className?: string;
 }
 
 const VerticalMarginsLayout: FC<VerticalMarginsLayoutProps> = ({
   children,
-}) => <div className="px-5">{children}</div>;
+  className = "",
+}) => <div className={["px-5", className].join(" ")}>{children}</div>;
 
 export default VerticalMarginsLayout;
