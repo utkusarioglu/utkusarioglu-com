01856657530528d61d8aee4ef96ee7201fab0497
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-09-05 09:40:22 +0000

    Chore(constants): Rename color

--- src/constants.tsx
+++ src/constants.tsx
@@ -136,7 +136,7 @@ export const HEX = {
 };
 
 const COLOR_CLASSES = {
-  bg: "bg-base-light dark:bg-base-dark",
+  baseBg: "bg-base-light dark:bg-base-dark",
   baseText: "text-base-light dark:text-base-dark",
 
   page: "text-primary-light dark:text-primary-dark",
