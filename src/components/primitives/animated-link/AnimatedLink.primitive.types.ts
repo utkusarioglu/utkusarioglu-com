6ecc06661fd56207a7054667cf4b64a0909232ef
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-11-05 06:00:59 +0000

    Feat: Theme compatibility for `AnimatedLink`
    - Alter `AnimatedLink` to check for active theme and change config
      accordingly.

--- src/components/primitives/animated-link/AnimatedLink.primitive.types.ts
+++ src/components/primitives/animated-link/AnimatedLink.primitive.types.ts
@@ -1,6 +1,7 @@
-import { type PropsWithChildren } from "react";
+import { type CSSProperties, type PropsWithChildren } from "react";
 
 export type AnimatedLinkProps = PropsWithChildren<{
   href: string;
   paddingAndMargins: string;
+  style?: CSSProperties;
 }>;
