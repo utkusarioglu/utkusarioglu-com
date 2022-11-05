0b02b03560f9e57a6fc13773725c8dd9200dfee8
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-11-05 05:51:36 +0000

    Chore: Minor changes for cleanup
    - Add `MAGIC` comment for two values that are magic numbers dependent on
      font size and type
    - Refactor a forgotten classname assignment that doesn't use `c`.
    
    [no ci]

--- src/utils/positioning.utils.ts
+++ src/utils/positioning.utils.ts
@@ -1,7 +1,7 @@
 export function homeNavX(window: Window): number {
-  return window.innerWidth / 2 - 140;
+  return window.innerWidth / 2 - 140; // MAGIC
 }
 
 export function homeNavY(window: Window): number {
-  return window.innerHeight / 2 + 160;
+  return window.innerHeight / 2 + 160; // MAGIC
 }
