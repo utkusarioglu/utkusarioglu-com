39fda35c7db68f149c523b91ace2c12b31c146bd
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-11-06 14:40:53 +0000

    Chore(types): About page type adjustments
    - Move `AboutPageProps` out of `types` folder and back into the
      component file. This was an oversight. The type belongs with the
      component.
    - Rename `about.types.tsx` as `about.types.ts`. This was an oversight
      during automated refactoring. This file is not a react component.
    - Add new content type `Named` that adds `name` attribute to yaml files.

--- src/types/about.types.ts
+++ src/types/about.types.ts
@@ -1,18 +1,20 @@
+import { type Named } from "./content.types";
+
 interface AboutParagraph {
   type: "paragraph";
   content: string;
 }
+
 interface AboutImage {
   type: "image";
   alt: string;
   credits: string;
   src: string;
 }
+
 type AboutItems = AboutParagraph | AboutImage;
+
 export type About = {
-  name: string;
+  page: "About";
   items: AboutItems[];
-};
-export interface AboutPageProps {
-  about: About;
-}
+} & Named;
