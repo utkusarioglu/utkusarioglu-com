300183861a891ba347aa93db3f2325b6d7906e65
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-11-10 14:57:40 +0000

    Chore(certs): Update certs paths
    - Update folder structure used for certs. The changes in this commit
      reflect changes that happen on gitignored files, and CI files that are
      created and destroyed during the runtime of jobs. Certs now all follow
      the same folder structure at all levels, hence this change is
      required.
    
    [no ci]

--- scripts/start-ssl-proxy.sh
+++ scripts/start-ssl-proxy.sh
@@ -4,5 +4,5 @@ echo "Starting local-ssl-proxy"
 yarn local-ssl-proxy \
   --source 443 \
   --target 3000 \
-  --key ./.certs/server/server.key \
-  --cert ./.certs/server/server.crt
+  --key ./.certs/server/private/server.key \
+  --cert ./.certs/server/certs/chain.crt
