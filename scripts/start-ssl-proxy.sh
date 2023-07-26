5f4d516e60040516723daee60068825aaf193915
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-07-26 11:02:19 +0000

    experiment/fix/pdf-pipeline/10

--- scripts/start-ssl-proxy.sh
+++ scripts/start-ssl-proxy.sh
@@ -6,7 +6,3 @@ yarn local-ssl-proxy \
   --target 3000 \
   --key ./.certs/server/tls.key \
   --cert ./.certs/server/chain.crt
-
-curl www.utkusarioglu.com
-curl localhost:3000
-curl 127.0.0.1:3000
