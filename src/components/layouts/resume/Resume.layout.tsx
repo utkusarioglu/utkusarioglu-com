d2f87a45db133bbb21b1614023187123359edea7
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-08-11 09:12:35 +0000

    Feat: Add Resume specialty section
    - Add resume specialty section in `assets/resume.yml`.
    - Add resume specialty selection in resume page. This will now connects
      with resume layout to propagate current resume layout for both screen
      and print.

--- src/components/layouts/resume/Resume.layout.tsx
+++ src/components/layouts/resume/Resume.layout.tsx
@@ -1,17 +1,27 @@
-import { type FC } from "react";
-import { type Resume } from "_types/resume.types";
+import { type FC, useEffect, useState } from "react";
 import ResumePrintLayout from "./ResumePrint.layout";
 import ResumeScreenLayout from "./ResumeScreen.layout";
+import { type SpecialtyId, type Resume } from "_types/resume.types";
 
 export interface ResumeLayoutProps {
   resume: Resume;
 }
 
 const ResumeLayout: FC<ResumeLayoutProps> = ({ resume }) => {
+  const [activeSpecialtyId, setActiveSpecialtyId] =
+    useState<SpecialtyId>("all");
+
   return (
     <>
-      <ResumeScreenLayout resume={resume} />
-      <ResumePrintLayout resume={resume} />
+      <ResumeScreenLayout
+        resume={resume}
+        activeSpecialtyId={activeSpecialtyId}
+        setActiveSpecialtyId={setActiveSpecialtyId}
+      />
+      <ResumePrintLayout
+        resume={resume}
+        activeSpecialtyId={activeSpecialtyId}
+      />
     </>
   );
 };
