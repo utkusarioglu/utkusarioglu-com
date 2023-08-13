d0f65af4cc31ffbe73953c30aea50198af731d3b
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-08-13 06:41:10 +0000

    experiment/feat/multi-resume/1

--- .puppeteer/src/index.js
+++ .puppeteer/src/index.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 
 const artifactsPath = "/home/pptruser/artifacts";
-const url = "http://www.utkusarioglu.com:3000/resume";
+const baseUrl = "http://www.utkusarioglu.com:3000/resume";
 
 const PAPER_FORMAT_VARIANTS = [
   {
@@ -20,26 +20,49 @@ const PAPER_FORMAT_VARIANTS = [
   },
 ];
 
-(async () => {
-  const browser = await puppeteer.launch({
-    args: [
-      // TODO get rid of this
-      "--ignore-certificate-errors",
-    ],
-  });
-  const page = await browser.newPage();
-  page.setViewport({ width: 1920, height: 1080 });
+const SPECIALTY_VARIANTS = ["fe", "be", "fs", "w3", "al"];
+
+const PHOTO_VARIANTS = [true, false];
+
+function createMatrix(specialtyIds, photoVariants)
+{
+  matrix = []
+  specialtyIds.forEach((specialtyId) =>
+  {
+    photoVariants.forEach((includePhoto) =>
+    {
+      matrix.push({specialtyId, includePhoto})
+    })
+  })
+  return matrix;
+}
+
+function createSingle(specialtyId, includePhoto) {
+  const url = [
+    baseUrl,
+    "?",
+    `specialtyId=${specialtyId}`,
+    "&",
+    `include-photo=${includePhoto ? "true" : "false"}`
+  ].join("")
+  const resumeCode = [specialtyId, includePhoto ? "p": "n"].join("")
+  const resumePath = `${artifactsPath}/raw/resume-${format}-${resumeCode}.pdf`;
+  const screenshotPath = `${artifactsPath}/screenshots/resume-${resumeCode}.png`;
+  console.log({baseUrl, url, specialtyId, includePhoto, resumeCode})
+
   await page.goto(url, {
     waitUntil: "networkidle2",
   });
-  await page.screenshot({ path: `${artifactsPath}/screenshots/resume.png` });
+  await page.screenshot({
+    path: screenshotPath,
+  });
 
   await PAPER_FORMAT_VARIANTS.reduce(async (chain, { format, margin }) => {
     chain = chain.then(() =>
       page.pdf({
         displayHeaderFooter: false,
         omitBackground: true,
-        path: `${artifactsPath}/raw/resume-${format}.pdf`,
+        path: resumePath,
         format,
         margin: {
           left: margin.x,
@@ -51,6 +74,22 @@ const PAPER_FORMAT_VARIANTS = [
     );
     return chain;
   }, Promise.resolve());
+}
+
+(async () => {
+  const browser = await puppeteer.launch({
+    args: [
+      // TODO get rid of this
+      "--ignore-certificate-errors",
+    ],
+  });
+  const page = await browser.newPage();
+  page.setViewport({ width: 1920, height: 1080 });
+
+  const matrix = createMatrix(SPECIALTY_VARIANTS, PHOTO_VARIANTS);
+  await matrix.reduce(({specialtyId, includePhoto}) => {
+    return createSingle(specialtyId, includePhoto)
+  }, Promise.resolve() )
 
   await browser.close();
 })();
