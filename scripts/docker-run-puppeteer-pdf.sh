86d27e3d55e8da252410d6eabb3452c45a7be027
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-08-13 09:25:47 +0000

    experiment/feat/multi-resume/14

--- scripts/docker-run-puppeteer-pdf.sh
+++ scripts/docker-run-puppeteer-pdf.sh
@@ -68,7 +68,7 @@ run_gs() {
           -dNOPAUSE \
           -dQUIET \
           -dBATCH \
-          -dDownScaleFactor=3 \
+          -r300 \
           -sOutputFile=$output_file \
           "$host_raw_artifacts_path/$source"
       done
